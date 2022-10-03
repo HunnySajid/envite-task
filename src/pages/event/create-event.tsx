1695303ddc3d83d4c4e05cca2e3c0a5b5ae21324
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2022-10-04 04:57:05 +0500

    add create event page

--- src/pages/event/create-event.tsx
+++ src/pages/event/create-event.tsx
@@ -14,10 +14,23 @@ import { Logo, FileUpload } from '../../components';
 import { Icon } from '@chakra-ui/react';
 import { BsCalendar } from 'react-icons/bs';
 import { GoLocation } from 'react-icons/go';
+import { useHistory } from 'react-router-dom';
 
 export const CreateEventForm = () => {
   const [img, setImg] = useState('');
+  const [name, setName] = useState('');
+  const [org, setOrg] = useState('');
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
     
+  const history = useHistory()
+
+  const handleClick = () => {
+    history.push('/event');
+    // TODO => BUG: history not updating the page for some reason
+    window.location.reload();
+}
+
   return (
     <Grid minH='100vh' p={0}>
       <Container px={{ base: 0, md: 40 }} py={{ base: 0, md: 20 }} maxW={'3xl'}>
@@ -36,6 +49,7 @@ export const CreateEventForm = () => {
           <Input
             textAlign={'left'}
             placeholder='Event Name'
+            onChange={(e) => setName(e.target.value) }
             fontWeight={700}
             border='none'
             px={2}
@@ -54,6 +68,7 @@ export const CreateEventForm = () => {
               textAlign={'left'}
               fontWeight={700}
               border='none'
+              onChange={(e) => setOrg(e.target.value) }
               placeholder='eg. Travis John'
               pr={2}
               fontSize={{ base: '14px', sm: '14px', md: '18px' }}
@@ -88,6 +103,8 @@ export const CreateEventForm = () => {
                   size='md'
                   border='none'
                   width={{ base: '200px', md: '300px' }}
+                  onChange={(e) => setStart(e.target.value) }
+            
                   fontSize={{ sm: 'md', md: 'lg' }}
                   fontWeight={700}
                 />
@@ -106,6 +123,7 @@ export const CreateEventForm = () => {
                   placeholder='Select End Date'
                   color='primary.100'
                   size='sm'
+                  onChange={(e) => setEnd(e.target.value) }
                   border='none'
                   width={{ base: '200px', md: '300px' }}
                   fontSize={{ sm: 'sm', md: 'md' }}
@@ -151,10 +169,12 @@ export const CreateEventForm = () => {
           px={6}
           my={6}
           bgGradient='linear(to-r, primary.300, primary.400)'
+          onClick={handleClick}
           color='white'
           _hover={{
             bg: 'primary.300',
           }}
+          disabled={ !img || !name || !org}
         >
           🎉 Next
         </Button>
